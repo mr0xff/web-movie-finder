d68fc7630894f9f4266706dd58b0ca559aaac6e3
0xff <worktrabalho633@gmail.com>
2025-06-01 19:56:33 +0100

    add: setting up vitest runner

---
+++ __tests__/vitest.test.ts
@@ -0,0 +1,16 @@
+import { test, expect } from 'vitest';
+
+test("setup vitest runner...", ()=>{
+  expect(1+2).toBe(3);
+});
+
+test("random test runner implementation", ()=>{
+  const randInt = (fallbackNumber=1)=> fallbackNumber ?? Math.round(Math.random()*10);
+  
+  const MAGIC_NUMBER = [ 
+    randInt(), 
+    randInt()
+  ];
+
+  expect(MAGIC_NUMBER[0]).toBe(MAGIC_NUMBER[1]);
+});
