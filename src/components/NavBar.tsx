18637a1e1caa00f3c1d90cb8cf88822b3db993ae
0xff <worktrabalho633@gmail.com>
2025-06-02 19:10:17 +0100

    add: pagination func

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import { FiSearch,  } from "react-icons/fi";
 import { IoCloseOutline } from "react-icons/io5";
 import { SearchInputContext } from "@/lib/contextProvider";
 import { BiMoviePlay } from "react-icons/bi";
-import { useNavigate } from "react-router";
+import { useNavigate, NavLink } from "react-router";
 
 export default function NavBar() {
   const [query, setQuery] = useState("");
@@ -19,17 +19,20 @@ export default function NavBar() {
     e.preventDefault();
     navigate({
       pathname: '/finder',
-      search: `name=${query.trim()}`
+      search: `name=${query.trim()}&page=1`
     })
   };
 
   return (
     <nav className="bg-gray-500/10 backdrop-blur-md shadow-md fixed top-0 w-full z-50">
       <div className="max-w-7xl mx-auto px-4 py-3 flex flex-col md:flex-row items-center justify-between">
-        <div className="text-xl font-bold text-gray-800 text-white mb-3 md:mb-0 flex gap-x-3 items-center">
-          <BiMoviePlay className="size-15 text-red-500" />
-          <div>Movie<span className="text-red-500 text-3xl italic">F</span>inder</div>
-        </div>
+        <NavLink to="/">
+          <div className="text-xl font-bold text-gray-800 text-white mb-3 md:mb-0 flex gap-x-3 items-center">
+            <BiMoviePlay className="size-15 text-red-500" />
+            <div>Movie<span className="text-red-500 text-3xl italic">F</span>inder</div>
+          </div>
+        </NavLink>
+
         <form
           onSubmit={handleSearch}
           className="flex items-center w-full max-w-sm bg-black rounded-xl px-4 py-2 outline-2 outline-red-500"
--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,17 +1,51 @@
-import Button from "@/components/Button";
 import { IoChevronBackOutline } from "react-icons/io5";
 import { GrFormNext } from "react-icons/gr";
+import { useSearchParams } from "react-router";
+import { useEffect } from "react";
 
-export default function Pagination({ pages }: { pages: number }){
+export default function Pagination({ 
+  pages
+}: { 
+  pages: number; 
+}){
+  const [ search, setSearch ] = useSearchParams();
+  const page = Number(search.get("page")) ?? 1;
+
+  const nextPage = () => { 
+    if(page < pages){
+      search.set("page", (page+1).toString());
+      setSearch(search.toString());
+    }
+  }
+
+  const prevPage = () => {
+    if(page > 1){
+      search.set("page", (page - 1).toString());
+      setSearch(search.toString());
+    }
+  }
+
+  useEffect(()=> {
+    if(page < 1){
+      search.set("page", "1");
+      setSearch(search.toString());
+    }else {
+      if (page > pages){
+        search.set("page", pages.toString());
+        setSearch(search.toString());
+      }
+    }
+  }, []);
+
   return(
     <div className="fixed bottom-0 w-full bg-black flex justify-between z-50 px-8 items-center py-3">
-      <button className="flex gap-x-2 items-center">
+      <button onClick={prevPage} disabled={page <= 1} className="flex gap-x-2 items-center disabled:opacity-50">
         <IoChevronBackOutline className="size-7 text-white active:text-red-500 hover:text-red-500" />
       </button>
-       <button className="flex gap-x-2 items-center hover:text-red-500">
+       <button onClick={nextPage} className="flex gap-x-2 items-center hover:text-red-500">
           <GrFormNext className="size-8 text-white active:text-red-500 hover:text-red-500" />
       </button>  
-      <p>3 de {pages}</p> 
+      <p className="font-bold">{page} de {pages}</p> 
     </div>
   )
 }
