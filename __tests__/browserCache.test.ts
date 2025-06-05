fed93fad641726ceeab8ecb9c15b7ea039a374aa
0xff <worktrabalho633@gmail.com>
2025-06-05 22:36:05 +0100

    update: unit test

--- __tests__/browserCache.test.ts
+++ __tests__/browserCache.test.ts
@@ -1,15 +1,16 @@
 import { expect, test, describe } from 'vitest';
 import { BrowserCache } from '../src/lib/utils';
+const bcache = new BrowserCache();
 
 describe("browser cache client", ()=> {
-  const bcache = new BrowserCache();
   
   test("toplist method", ()=> {
     bcache.updateTopList([ 1, 3, 2]);
     expect(bcache.getTopList()).toMatchObject([1, 3, 2]);
   });
 
-  test("viewed items", ()=>{
-    
-  });
+  // test("viewed items", ()=>{
+  //   bcache.updateViewed({ imdbId: "testId" });
+  //   expect(bcache.getViewed("testId")).toMatchObject({ imdbId: "testId" });
+  // });
 })
