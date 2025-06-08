b80405e3285bb39e8264a6162ccb9c7cca364a2e
0xff <worktrabalho633@gmail.com>
2025-06-08 22:03:20 +0100

    fix: base path to github pages

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -7,7 +7,7 @@ import MovieDetails from "@/routes/MovieDetails";
 
 export default function Router(){
   return(
-    <BrowserRouter>
+    <BrowserRouter basename="web-movie-finder">
       <Routes>
         <Route element={<Layout />}>
           <Route index element={<Home />} />
